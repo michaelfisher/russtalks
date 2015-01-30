e33eac08851638d958a52e637ad0f72994ff85f5
Michael Fisher <michael@thefisherhome.net>
2015-01-30 12:48:43 -0600

    Third commit.

--- inc/signup.php
+++ inc/signup.php
@@ -1,11 +1,12 @@
 <?php
 
 	// Configuration 
-    $to = "michael.fisher.mobile@gmail.com"; // Your email address. 
-    $subject = "New signup via the Signup Landing Page"; // Email subject line 
+    $to = "michael@thefisherhome.net"; // Your email address. 
+    $subject = "New Signup from RussTalks.com"; // Email subject line 
     
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name']; 
+    $email = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $from = "$name <$email>";
     $headers = "From: $from";
 
     $fields = array();
@@ -15,5 +16,8 @@
     $body = "Details:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%s: %s\n",$b,$_REQUEST[$a]); }
 
     $send = mail($to, $subject, $body, $headers);
+
+    $file = $_SERVER['DOCUMENT_ROOT'] . "/" . "logs" . "/" . $email;
+    file_put_contents($file, $name);
 
 ?>
